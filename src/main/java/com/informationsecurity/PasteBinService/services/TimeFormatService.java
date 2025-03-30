4d05bf33a2e80d01aa2ebcf93c2c31dc9a149d4e
Sunrise <ieg-v@mail.ru>
2025-03-30 17:10:53 +0700

    Рефакторинг TimeFormatService

--- src/main/java/com/informationsecurity/PasteBinService/services/TimeFormatService.java
+++ src/main/java/com/informationsecurity/PasteBinService/services/TimeFormatService.java
@@ -11,34 +11,25 @@ public class TimeFormatService {
 
     public String getTimeFormat(LocalDateTime time) {
         String[] s = DateTimeFormatter.ofPattern("dd_MM_yyyy_hh_mm").format(time).split("_");
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder
-                .append(s[0]).append(" ")
-                .append(getMonth(s[1])).append(" ")
-                .append(s[2])
-                .append(", ")
-                .append(s[3]).append(":")
-                .append(s[4]);
-        return stringBuilder.toString();
+        return "%s %s %s, %s:%s".formatted(s[0], getMonth(s[1]), s[2], s[3], s[4]);
     }
 
     private String getMonth(String month) {
-        String result = "";
-        switch (month) {
-            case "01" -> result = Months.JAN.getName();
-            case "02" -> result = Months.FEB.getName();
-            case "03" -> result = Months.MAR.getName();
-            case "04" -> result = Months.APR.getName();
-            case "05" -> result = Months.MAY.getName();
-            case "06" -> result = Months.JUN.getName();
-            case "07" -> result = Months.JUL.getName();
-            case "08" -> result = Months.AUG.getName();
-            case "09" -> result = Months.SEP.getName();
-            case "10" -> result = Months.OCT.getName();
-            case "11" -> result = Months.NOV.getName();
-            case "12" -> result = Months.DEC.getName();
-        }
-        return result;
+        return switch (month) {
+            case "01" -> Months.JAN.getName();
+            case "02" -> Months.FEB.getName();
+            case "03" -> Months.MAR.getName();
+            case "04" -> Months.APR.getName();
+            case "05" -> Months.MAY.getName();
+            case "06" -> Months.JUN.getName();
+            case "07" -> Months.JUL.getName();
+            case "08" -> Months.AUG.getName();
+            case "09" -> Months.SEP.getName();
+            case "10" -> Months.OCT.getName();
+            case "11" -> Months.NOV.getName();
+            case "12" -> Months.DEC.getName();
+            default -> "";
+        };
     }
 
 }
